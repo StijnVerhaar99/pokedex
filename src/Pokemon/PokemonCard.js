90dd9e6c604296e9a1c7266739a580fd713d6a94
Stijn Verhaar <39126867+StijnVerhaar1999@users.noreply.github.com>
2019-11-19 22:20:00 +0100

    first commit

---
+++ src/Pokemon/PokemonCard.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class PokemonCard extends Component {
+    state = {
+        name : ' ',
+        imageUrl : ' ',
+        pokemonIndex : ' '
+    };
+
+    componentDidMount() {
+        const name = this.props.name;
+        const url = this.props.url;
+        const pokemonIndex = url.split('/')[url.split('/').length - 2 ];
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`
+
+        this.setState ({
+            name,
+            imageUrl,
+            pokemonIndex
+        });
+    }
+    
+    render() {
+        return ( 
+            <div>
+                <h1>{this.state.name}</h1>
+                <p>{this.state.pokemonIndex}</p>
+                <img src={this.state.imageUrl} alt=''/>
+            </div>
+        );
+    }
+}
+
+export default PokemonCard;
